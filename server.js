3f17e0e09dd2ad33f12f5cf3c34e033cc6a47396
Michael Manduca <michael@mandu.ca>
2014-10-25 02:40:15 -0700

    sendFile index.html

--- server.js
+++ server.js
@@ -5,8 +5,37 @@
 var express = require('express')
   , app     = express()
 
-app.get('/', function (req, res) {
-  res.send("Hello World")
+app.use(express.static(__dirname + '/'))
+
+app.use(function (req, res, next) {
+  console.log('Time: %d', Date.now())
+  next()
 })
 
-app.listen(3000)
+// Routes
+  
+app.get('/', function (req, res) {
+  res.sendFile("./index.html")
+})
+  
+app.post('/', function (req, res) {
+  res.send("Got a POST request")
+})
+  
+app.put('/api', function (req, res) {
+  res.send("Got a PUT request at /api")
+})
+  
+app.delete('/api/user/:id', function (req, res) {
+  res.send("Got a DELETE request")
+})
+
+
+// Server Configuration
+var server = app.listen(3000, function() {
+
+  var host = server.address().address
+    , port = server.address().port
+
+  console.log('Listening at http://%s:%s', host, port)   
+})
