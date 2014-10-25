4e3857bfce1339dc9e60bbb1fdf5573fa32d6570
Michael Manduca <michael@mandu.ca>
2014-10-25 02:07:32 -0700

    Output Hello World\!

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 // NodeJS REST Server
 
+'use strict'
 // Initialize the core components in Express
+var express = require('express')
+  , app     = express()
+
+app.get('/', function (req, res) {
+  res.send("Hello World")
+})
+
+app.listen(3000)
